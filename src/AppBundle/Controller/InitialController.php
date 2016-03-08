d66bd0199b8aecb3d18054178f72dd3b338aa19b
Arnaud Simon <asimon@20minutes.fr>
2016-03-08 13:49:52 +0100

    mail initial

--- src/AppBundle/Controller/InitialController.php
+++ src/AppBundle/Controller/InitialController.php
@@ -73,7 +73,7 @@ class InitialController extends Controller
                         $message = $this->renderView('AppBundle:Emails:packPreparation.html.twig', array('pack' => $pack));
                         $destinataire = $this->container->getParameter('sender');
                         $headers = "From: \"".$this->container->getParameter('sender')."\"<moi@domaine.com>\n";
-                        $headers .= "Reply-To: "..$this->container->getParameter('sender')"\n";
+                        $headers .= "Reply-To: ".$this->container->getParameter('sender')"\n";
                         $headers .= "Content-Type: text/html; charset=\"iso-8859-1\"";
                         mail($destinataire,$sujet,$message,$headers);
                     }                    
