b5ac8646283c2e3a7fc042adaf8b585ffda37a45
105863 <u52639231@false.(none)>
2016-03-22 15:29:00 +0100

    preprod

--- src/AppBundle/Command/ReminderCommand.php
+++ src/AppBundle/Command/ReminderCommand.php
@@ -42,7 +42,7 @@ class ReminderCommand extends ContainerAwareCommand
         //Reminder four weeks result with 3 days late
         foreach ($results as $result) {
 
-            $email = ($result->getPack()->getPackType() == Pack::THEME ) ? $this->getContainer()->getParameter('sender_themes') : $this->getContainer()->getParameter('sender_custom');
+            $email = ($result->getPack()->getPackType() == Pack::THEME) ? $this->getContainer()->getParameter('sender_themes') : $this->getContainer()->getParameter('sender_custom');
             $sujet = 'David costa : Rappel bilan à 4 semaines';
             $message = $this->getContainer()->get('templating')->render('AppBundle:Emails:reminderFourWeeksSinceThreeDays.html.twig', array('result' => $result));
             $destinataire = $result->getPack()->getInitial()->getEmail();
@@ -60,7 +60,7 @@ class ReminderCommand extends ContainerAwareCommand
             foreach ($results as $result) {
             if ($result->getPack()->getDaysLeft() > 24) {
 
-            $email = ($result->getPack()->getPackType() == Pack::THEME ) ? $this->getContainer()->getParameter('sender_themes') : $this->getContainer()->getParameter('sender_custom');
+            $email = ($result->getPack()->getPackType() == Pack::THEME�) ? $this->getContainer()->getParameter('sender_themes') : $this->getContainer()->getParameter('sender_custom');
             $sujet = 'David costa : Rappel bilan à 4 semaines';
             $message = $this->getContainer()->get('templating')->render('AppBundle:Emails:reminderFourWeeksSinceSevenDays.html.twig', array('result' => $result));
             $destinataire = $result->getPack()->getInitial()->getEmail();
@@ -95,7 +95,7 @@ class ReminderCommand extends ContainerAwareCommand
         //Reminder esthetic result with 7 days late
         foreach ($results as $result) {
 
-            $email = ($result->getPack()->getPackType() == Pack::THEME ) ? $this->getContainer()->getParameter('sender_themes') : $this->getContainer()->getParameter('sender_custom');
+            $email = ($result->getPack()->getPackType() == Pack::THEME) ? $this->getContainer()->getParameter('sender_themes') : $this->getContainer()->getParameter('sender_custom');
             $sujet = 'David costa : Rappel bilan à 3 mois';
             $message = $this->getContainer()->get('templating')->render('AppBundle:Emails:reminderEstheticSinceSevenDays.html.twig', array('result' => $result));
             $destinataire = $result->getPack()->getInitial()->getEmail();
