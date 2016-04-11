929bcee5c81329656e9174703340569e4668a8a0
Arnaud Simon <asimon@20minutes.fr>
2016-04-11 18:46:34 +0200

    page statique

---
+++ src/AppBundle/Controller/ScaleController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * Scale controller.
+ *
+ * @Route("/bareme")
+ */
+class ScaleController extends Controller
+{
+    /**
+     *
+     * @Route("/", name="scale_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        return $this->render('AppBundle:Scale:index.html.twig');
+    }
+
+}
