c29f606c9b6e97e242787f93084c69891bdc1f36
Arnaud Simon <asimon@20minutes.fr>
2016-04-22 15:57:42 +0200

    fancybox

--- src/AppBundle/Resources/public/js/scripts.js
+++ src/AppBundle/Resources/public/js/scripts.js
@@ -3,7 +3,29 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+function readURL(input, preview) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
 
+        reader.onload = function (e) {
+            preview.attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$("#initial_photoFront").change(function () {
+    readURL(this, $(this).next('img'));
+});
+$("#initial_photoSide").change(function () {
+    readURL(this, $(this).next('img'));
+});
+$("#initial_photoBack").change(function () {
+    readURL(this, $(this).next('img'));
+});
+
+$(".fancybox").fancybox();
 
 
 
