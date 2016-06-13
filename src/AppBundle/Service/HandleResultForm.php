353618f632636aeaebfb884513ee608efc448f22
Arnaud Simon <asimon@20minutes.fr>
2016-06-13 18:16:02 +0200

    notifications for results

--- src/AppBundle/Service/HandleResultForm.php
+++ src/AppBundle/Service/HandleResultForm.php
@@ -38,7 +38,7 @@ class HandleResultForm
 
             $destinataire = ($result->getPack()->getPackType() == Pack::THEME) ? $this->container->getParameter('sender_themes') : $this->container->getParameter('sender_custom');
             $sujet = 'Pack en pause';
-            $message = $this->renderView('AppBundle:Emails:resultsCompleted.html.twig', array('result' => $result));
+            $message = $this->templating->render('AppBundle:Emails:resultsCompleted.html.twig', array('result' => $result));
             $headers = "From: \"".$this->container->getParameter('sender_app')."\"<".$this->container->getParameter('sender_app').">\n";
             $headers .= "Reply-To: ".$this->container->getParameter('sender_app')."\n";
             $headers .= "Content-Type: text/html; charset=\"iso-8859-1\"";
